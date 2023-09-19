e7a572ce39889897170b3a87c60e8546731fa34a
audy.pratama <audy.pratama@mashroom.com>
2023-09-19 10:13:26 +0700

    try vercel ci/cd

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,10 +5,10 @@ export default function Home() {
   const navigate = useNavigate();
   return (
     <div className='border-black border-2 p-4'>
-      <h4 className='border-b-2 py-2'>New Testing Example with vercel</h4>
+      <h4 className='border-b-2 py-2'>New Testing Example with vercel v2</h4>
 
       <div className='my-2 bg-slate-50'>
-        <h1>Why do we need Test ? for now</h1>
+        <h1>Why do we need Test ?</h1>
         <p>
           To guarantee our code quality. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate qui
           doloremque magni odit quia, fugit autem. Voluptates earum at assumenda quo nulla! At illo laboriosam tenetur
